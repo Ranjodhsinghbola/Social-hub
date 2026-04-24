daff480b9f98cfc67ddebbe9a10dceb3d22a7362
Bola Ranjodh Singh <ranjodhsinghbola@gmail.com>
2026-04-24 09:52:22 +0200

    Remove app folder from main

--- app/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-?>
-<script>
-    localStorage.removeItem('user_id');
-    localStorage.removeItem('username');
-    window.location.href = 'login.html';
-</script>
